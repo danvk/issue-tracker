c6564488068af8ee5868c702d6ac32d044f4f870
Dan Vanderkam <danvdk@gmail.com>
2015-10-13 18:36:14 -0400

    use docopt & backfill pulls

--- backfill.py
+++ backfill.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""Backfill counts for the GitHub Issue Tracker.
 
-import dateutil.parser
+Usage:
+  backfill.py <user> <repo> [--all | --issues | --pulls | --stars | --labels ]
+
+Options:
+  -h --help    Show this screen.
+  --version    Show version.
+  --all        Backfill all counts. [default: true]
+  --issues     Backfill only open issues.
+  --pulls      Backfill only open pull requests.
+  --stars      Backfill only stargazer counts.
+  --labels     Backfill only per-label open issue counts.
+"""
+
 import glob
 import os
 import json
 import sys
 from collections import defaultdict
 from datetime import datetime, timedelta
+
+import dateutil.parser
+from docopt import docopt
 
 import github
 import tracker
@@ -24,6 +40,7 @@ CACHE_DIR = 'issue-tracker-backfill'
 LABEL_RENAMES = {}
 
 def fetch_full_issue(issue):
+    """Fills out the events field for an Issue or Pull Request."""
     print 'Fetching issue %d...' % issue.number
     issue_json = issue.raw_data
     issue_json['events'] = [event.raw_data for event in issue.get_events()]
@@ -51,8 +68,27 @@ def fetch_all_issues(repo):
         else:
             issue_data = fetch_full_issue(issue)
             json.dump(issue_data, open(cache_file, 'w'), indent=2, sort_keys=True)
-        issues.append(issue_data)
+        if 'pull_request' not in issue_data:
+            issues.append(issue_data)
     return issues
+
+
+def fetch_all_pulls(repo):
+    '''Fetch all open/closed PRs for a repo.
+
+    This returns the issue views of the PRs, which include events (unlike the PR views).
+    '''
+    prs = []
+    for pr in repo.get_pulls(state='all'):
+        cache_file = os.path.join(CACHE_DIR, '%d.json' % pr.number)
+        if os.path.exists(cache_file):
+            pr_data = json.load(open(cache_file))
+        else:
+            issue = repo.get_issue(pr.number)
+            pr_data = fetch_full_issue(issue)
+            json.dump(pr_data, open(cache_file, 'w'), indent=2, sort_keys=True)
+        prs.append(pr_data)
+    return prs
 
 
 def fetch_all_issues_from_cache():
@@ -71,7 +107,7 @@ def needs_synthetic_close(issue):
     return is_open and issue['state'] == 'closed'
 
 
-def issue_events(issue):
+def issue_events(issue, track_labels=True):
     """Returns a list of (time, label or None, delta) events.
     
     None = all issues
@@ -84,10 +120,6 @@ def issue_events(issue):
     events = []
     events.append((issue['created_at'], None, +1))
 
-    # XXX do open pull requests count toward the total # of open issues?
-    if 'pull_request' in issue:
-        return []
-
     raw_events = issue['events']
     if needs_synthetic_close(issue):
         raw_events = raw_events + [{
@@ -98,13 +130,13 @@ def issue_events(issue):
     for event in raw_events:
         t = event['event']
         time = event['created_at']
-        if t == 'labeled':
+        if t == 'labeled' and track_labels:
             label = event['label']['name']
             label = LABEL_RENAMES.get(label, label)
             labels.add(label)
             if is_open:
                 events.append((time, label, +1))
-        elif t == 'unlabeled':
+        elif t == 'unlabeled' and track_labels:
             label = event['label']['name']
             label = LABEL_RENAMES.get(label, label)
             labels.remove(label)
@@ -112,19 +144,23 @@ def issue_events(issue):
                 events.append((time, label, -1))
         elif t == 'closed':
             is_open = False
-            for label in reversed([None] + list(labels)):
-                events.append((time, label, -1))
+            events.append((time, None, -1))
+            if track_labels:
+                for label in reversed(list(labels)):
+                    events.append((time, label, -1))
         elif t == 'reopened':
             is_open = True
-            for label in [None] + list(labels):
+            events.append((time, None, +1))
+            for label in list(labels):
                 events.append((time, label, +1))
 
-    github_labels = {label['name'] for label in issue['labels']}
-    if labels != github_labels:
-        print 'Label mismatch for issue %d' % issue['number']
-        print '  Computed %s' % labels
-        print '  GitHub says %s' % github_labels
-        print 'You may need to fill out the LABEL_RENAMES variable\n'
+    if track_labels:
+        github_labels = {label['name'] for label in issue['labels']}
+        if labels != github_labels:
+            print 'Label mismatch for issue %d' % issue['number']
+            print '  Computed %s' % labels
+            print '  GitHub says %s' % github_labels
+            print 'You may need to fill out the LABEL_RENAMES variable\n'
     return events
 
 
@@ -167,26 +203,10 @@ def summarize_rate_limit(g):
     sys.stderr.write('\n')
 
 
-if __name__ == '__main__':
-    owner, repo_name = sys.argv[1:]
-    CACHE_DIR += '-%s-%s' % (owner, repo_name)
-
-    if not os.path.exists(CACHE_DIR):
-        os.mkdir(CACHE_DIR)
-    g = tracker.get_github()
-
-    try:
-        repo = g.get_user(owner).get_repo(repo_name)
-        issues = fetch_all_issues(repo)
-    except github.GithubException as e:
-        if e.status == 403 and 'rate limit' in e.data['message']:
-            summarize_rate_limit(g)
-            raise e
-
-    # issues = fetch_all_issues_from_cache()
-    sys.stderr.write('Loaded %d issues\n' % len(issues))
-
-    all_events = flatten((issue_events(issue) for issue in issues))
+def backfill_core(issues, backfill_labels=False):
+    '''Shared backfilling logic between issues and pull requests.'''
+    all_events = flatten((issue_events(issue, track_labels=backfill_labels)
+                         for issue in issues))
     first_date = find_first_date(all_events)
     dates = all_dates(first_date)
 
@@ -206,13 +226,80 @@ if __name__ == '__main__':
     open_issues = by_label[None]
     del by_label[None]
 
-    # Chunk into reasonably-sized requests
-    objs = [
-        {'delete': 'open_issues'},
-        {'open_issues': open_issues},
-        {'delete': 'by_label'}
-    ] + [{'by_label': {label: by_label[label]}} for label in labels]
+    return open_issues, by_label
 
+
+def backfill_issues(g, owner, repo_name, backfill_labels=False):
+    repo = g.get_user(owner).get_repo(repo_name)
+    issues = fetch_all_issues(repo)
+
+    sys.stderr.write('Loaded %d issues\n' % len(issues))
+    return backfill_core(issues, backfill_labels=backfill_labels)
+
+
+def backfill_pulls(g, owner, repo_name):
+    repo = g.get_user(owner).get_repo(repo_name)
+    pulls = fetch_all_pulls(repo)
+    sys.stderr.write('Loaded %d pull requests\n' % len(pulls))
+    return backfill_core(pulls, backfill_labels=False)[0]
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Backfiller 0.1')
+    owner = arguments['<user>']
+    repo_name = arguments['<repo>']
+
+    CACHE_DIR += '-%s-%s' % (owner, repo_name)
+
+    if not os.path.exists(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
+    g = tracker.get_github()
+
+    open_issues = []
+    open_pulls = []
+    by_label = {}
+    stargazers = []
+
+    do_all = arguments['--all']
+    do_issues = arguments['--issues'] or do_all
+    do_labels = arguments['--labels'] or do_all
+    do_stars = arguments['--stars'] or do_all
+    do_pulls = arguments['--pulls'] or do_all
+
+    try:
+        if do_issues or do_labels:
+            open_issues, by_label = backfill_issues(g, owner, repo_name, backfill_labels=(do_all or do_labels))
+        if do_stars:
+            sys.stderr.write('\nStargazer backfilling is not implemented yet.\n\n')
+            pass
+        if do_pulls:
+            open_pulls = backfill_pulls(g, owner, repo_name)
+
+    except github.GithubException as e:
+        if e.status == 403 and 'rate limit' in e.data['message']:
+            summarize_rate_limit(g)
+            raise e
+
+    # Chunk into reasonably-sized requests
+    objs = []
+    if do_issues:
+        objs.extend([
+            {'delete': 'open_issues'},
+            {'open_issues': open_issues}
+        ])
+    if do_labels:
+        objs.append({'delete': 'by_label'})
+        objs.extend([{'by_label': {label: by_label[label]}} for label in labels])
+    if do_pulls:
+        objs.extend([
+            {'delete': 'open_pulls'},
+            {'open_pulls': open_pulls}
+        ])
+    if do_stars:
+        pass
+
+    for path in glob.glob('%s/backfill????.json' % CACHE_DIR):
+        os.remove(path)
     for i, obj in enumerate(objs):
         json.dump(obj, open('%s/backfill%04d.json' % (CACHE_DIR, i), 'w'))
 
